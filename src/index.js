eb58c0b0477c3852ce23feca57035e9b260cdcde
Emily Wairimu <emily.wairimu@student.moringaschool.com>
2023-03-22 14:31:49 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#tasks');
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  const newTask = document.createElement('li');
+  const taskDescription = document.querySelector('#new-task-description').value;
+  newTask.innerText = taskDescription;
+  taskList.appendChild(newTask);
+  form.reset();
 });
+
+})
